1b5480c6ab778a61232765506fdc6adc1d34a55e
Shubham <shubhamkharche2003@gmail.com>
2025-08-01 17:41:14 +0530

    Project setup completed and environment setup done

---
+++ src/main/java/com/shubham/Nexwent/Configuration/EnvLoader.java
@@ -0,0 +1,17 @@
+package com.shubham.Nexwent.Configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class EnvLoader implements EnvironmentPostProcessor {
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+        dotenv.entries()
+                .forEach(entry -> environment.getSystemProperties().put(entry.getKey(), entry.getValue()));
+    }
+}
