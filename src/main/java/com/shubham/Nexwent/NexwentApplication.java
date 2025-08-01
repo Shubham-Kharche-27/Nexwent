f173428305548c1b13c79b8a84726773f0d612d4
Shubham <shubhamkharche2003@gmail.com>
2025-08-01 17:28:12 +0530

    Project setup initiated

---
+++ src/main/java/com/shubham/Nexwent/NexwentApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.Nexwent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexwentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexwentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/Nexwent/NexwentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.Nexwent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexwentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
